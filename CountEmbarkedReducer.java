0e7656dcc3979cd9c924b5ce2078574fb663480c
Phạm Thành Trung <70435025+trungit2001@users.noreply.github.com>
2022-04-08 10:32:25 +0700

    add midterm gr1

---
+++ CountEmbarkedReducer.java
@@ -0,0 +1,18 @@
+package midterm;
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+
+public class CountEmbarkedReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
+			Reporter reporter) throws IOException {
+		int sum = 0;
+		while (values.hasNext()) {
+			sum += values.next().get();
+		}
+		output.collect(key, new IntWritable(sum));
+	}
+}
